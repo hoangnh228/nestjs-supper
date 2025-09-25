bfbb39514cc13723c823c820fd7a27c364c82cdc
Hoang Nguyen <hoang.nguyen@twenty-tech.com>
2025-09-25 14:42:53 +0700

    crud posts

--- src/routes/posts/posts.controller.ts
+++ src/routes/posts/posts.controller.ts
@@ -1,8 +1,9 @@
 import { AuthTypes, ConditionGuard } from './../../shared/constants/auth.constant'
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { PostsService } from './posts.service'
 import { Auth } from 'src/shared/decorators/auth.decorator'
 import { ActiveUser } from 'src/shared/decorators/active-user.decorator'
+import { CreatePostDTO, GetPostItemDTO, UpdatePostDTO } from 'src/routes/posts/post.dto'
 
 @Controller('posts')
 export class PostsController {
@@ -10,13 +11,31 @@ export class PostsController {
 
   @Auth([AuthTypes.Bearer, AuthTypes.ApiKey], { condition: ConditionGuard.Or })
   @Get()
-  getPosts() {
-    return this.postsService.getPosts()
+  getPosts(@ActiveUser('userId') userId: number) {
+    return this.postsService.getPosts(userId).then((posts) => posts.map((post) => new GetPostItemDTO(post)))
+  }
+
+  @Auth([AuthTypes.Bearer])
+  @Get(':id')
+  async getPostById(@Param('id') id: number) {
+    return new GetPostItemDTO(await this.postsService.getPostById(id))
   }
 
   @Auth([AuthTypes.Bearer])
   @Post()
-  createPost(@Body() body: any, @ActiveUser('userId') userId: number) {
-    return this.postsService.createPost(userId, body)
+  async createPost(@Body() body: CreatePostDTO, @ActiveUser('userId') userId: number) {
+    return new GetPostItemDTO(await this.postsService.createPost(userId, body))
+  }
+
+  @Auth([AuthTypes.Bearer])
+  @Put(':id')
+  async updatePost(@Param('id') id: number, @Body() body: UpdatePostDTO, @ActiveUser('userId') userId: number) {
+    return new GetPostItemDTO(await this.postsService.updatePost(userId, id, body))
+  }
+
+  @Auth([AuthTypes.Bearer])
+  @Delete(':id')
+  async deletePost(@Param('id') id: number, @ActiveUser('userId') userId: number): Promise<{ message: string }> {
+    return this.postsService.deletePost(userId, id)
   }
 }
--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CreatePostDTO, UpdatePostDTO } from 'src/routes/posts/post.dto'
+import { isNotFoundPrismaError } from 'src/shared/helpers'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  getPosts() {
+  getPosts(userId: number) {
     const createLogWithTime = (logFnc: (message: string) => void) => (message1: string) => {
       const now = new Date().toISOString()
       logFnc(`${now}: ${message1}`)
@@ -14,10 +16,43 @@ export class PostsService {
     const log1 = createLogWithTime(console.log)
     log1('Hello')
 
-    return this.prisma.post.findMany()
+    return this.prisma.post.findMany({
+      where: {
+        authorId: userId,
+      },
+      include: {
+        author: {
+          omit: {
+            password: true,
+          },
+        },
+      },
+    })
   }
 
-  createPost(userId: number, body: any) {
+  async getPostById(id: number) {
+    try {
+      return await this.prisma.post.findUniqueOrThrow({
+        where: {
+          id,
+        },
+        include: {
+          author: {
+            omit: {
+              password: true,
+            },
+          },
+        },
+      })
+    } catch (error) {
+      if (isNotFoundPrismaError(error)) {
+        throw new NotFoundException('Post not found')
+      }
+      throw error
+    }
+  }
+
+  createPost(userId: number, body: CreatePostDTO) {
     console.log(userId)
     return this.prisma.post.create({
       data: {
@@ -26,5 +61,49 @@ export class PostsService {
         authorId: userId,
       },
     })
+  }
+
+  async updatePost(userId: number, id: number, body: UpdatePostDTO) {
+    try {
+      return await this.prisma.post.update({
+        where: {
+          id,
+          authorId: userId,
+        },
+        data: {
+          title: body.title,
+          content: body.content,
+        },
+        include: {
+          author: {
+            omit: {
+              password: true,
+            },
+          },
+        },
+      })
+    } catch (error) {
+      if (isNotFoundPrismaError(error)) {
+        throw new NotFoundException('Post not found')
+      }
+      throw error
+    }
+  }
+
+  async deletePost(userId: number, id: number) {
+    try {
+      await this.prisma.post.delete({
+        where: {
+          id,
+          authorId: userId,
+        },
+      })
+      return { message: 'Post deleted successfully' }
+    } catch (error) {
+      if (isNotFoundPrismaError(error)) {
+        throw new NotFoundException('Post not found')
+      }
+      throw error
+    }
   }
 }
